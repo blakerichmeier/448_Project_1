2c24fa95eac28661681abcee8a973cf02da5f370
Alec McGlynn <alecmcglynn@ku.edu>
2021-10-09 17:16:39 -0500

    Added broken medium AI

--- ai.cpp
+++ ai.cpp
@@ -4,69 +4,128 @@ AI::AI()
 	initializeHitsArr();
 }
 
-
-
 /*
 @author Sam Jerguson
 */
-int* AI::aiMove(GameBoard& playerBoard)
+int *AI::aiMove(GameBoard &playerBoard)
 {
-  int* guess = new int[2];
-  switch(m_difficulty)
-  {
-    case 1:
-      //easy code here
-	  int rand_x,rand_y;
-	  srand(time(NULL));
-	  REDO:;
-	  for(int i = 0; i < 2; i++) {
-		  if (i == 1) {
-			  rand_x = rand() % 9;
-		  } else {
-			  rand_y = rand() % 10;
-		  }
-	  }
-	  if(hitsArr[rand_x][rand_y] == 1) {
-		  goto REDO;
-	  }
-	  guess[0] = rand_x;
-	  guess[1] = rand_y;
-	  hitsArr[rand_x][rand_y] = 1;
-	  return guess;
-      break;
-    case 2:
-      //medium code here
-      break;
-    case 3:
+	int rand_x, rand_y;
+	srand(time(NULL));
+	int *guess = new int[2];
+	switch (m_difficulty)
 	{
-      //hard code here
-	  for(int i = 0; i < 10; i++)
-	  {
-		  for(int j = 0; j < 11; j++)
-		  {
-			  if((playerBoard.get_shipArr()[i][j] == 'S') && (hitsArr[i][j] != 1)) //checks to ensure
-			  {
-				  guess[0] = i;
-				  guess[1] = j;
-				  hitsArr[i][j] = 1; //marks where the ai has already hit
-				  return guess; //returns an array containing the row and column of the ai's guess
-			  }
-		  }
-	  }
-	  return guess;
-	  
-      break;
+	case 1:
+	{
+	//easy code here
+	REDO:;
+		for (int i = 0; i < 2; i++)
+		{
+			if (i == 1)
+			{
+				rand_x = rand() % 9;
+			}
+			else
+			{
+				rand_y = rand() % 10;
+			}
+		}
+		if (hitsArr[rand_x][rand_y] == 1)
+		{
+			goto REDO;
+		}
+		guess[0] = rand_x;
+		guess[1] = rand_y;
+		hitsArr[rand_x][rand_y] = 1;
+		return guess;
+		break;
 	}
-    default:
-      cout << "Invalid difficulty\n";
-  }
+	case 2:
+	{
+		//medium code here
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+				std::cout << "i: " << i << " j: " << j << endl;
+				if (playerBoard.get_shipArr()[i][j] == 'S' && hitsArr[i][j] != 0)
+				{
+					std::cout << "found an hit ship at " << i << j << endl;
+					if (i > 0)
+					{
+						if (hitsArr[i - 1][j] == 0)
+						{
+							guess[0] = i - 1;
+							guess[1] = j;
+							hitsArr[i - 1][j] = 1;
+							return guess;
+						}
+					}
+					if (i < 10)
+					{
+						if (hitsArr[i + 1][j] == 0)
+						{
+							guess[0] = i + 1;
+							guess[1] = j;
+							hitsArr[i + 1][j] = 1;
+							return guess;
+						}
+					}
+					if (j > 0)
+					{
+						if (hitsArr[i][j - 1] == 0)
+						{
+							guess[0] = i;
+							guess[1] = j - 1;
+							hitsArr[i][j - 1] = 1;
+							return guess;
+						}
+					}
+					if (j < 11)
+					{
+						if (hitsArr[i][j + 1] == 0)
+						{
+							guess[0] = i;
+							guess[1] = j + 1;
+							hitsArr[i][j + 1] = 1;
+							return guess;
+						}
+					}
+				}
+			}
+		}
+		goto REDO; //uses easy AI for random guesses
+		break;
+	}
+	case 3:
+	{
+		//hard code here
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+				if ((playerBoard.get_shipArr()[i][j] == 'S') && (hitsArr[i][j] != 1)) //checks to ensure
+				{
+					guess[0] = i;
+					guess[1] = j;
+					hitsArr[i][j] = 1; //marks where the ai has already hit
+					return guess;	   //returns an array containing the row and column of the ai's guess
+				}
+			}
+		}
+		return guess;
+
+		break;
+	}
+	default:
+		cout << "Invalid difficulty\n";
+	}
 }
 
 void AI::initializeHitsArr()
 {
-	for(int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		for(int j = 0; j < 11; j++)
+		for (int j = 0; j < 11; j++)
 		{
 			hitsArr[i][j] = 0;
 		}
@@ -82,4 +141,3 @@ int AI::getDifficulty()
 {
 	return m_difficulty;
 }
-
